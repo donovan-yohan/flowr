daf460fa60387dcca0c4d22f3b7bb40fb4897aa7
Donovan Yohan <donovan.yohan@cgic.cooperators.ca>
2019-07-27 01:20:20 -0400

    Remove ui state slices from localStorage

--- plugins/vuex-persist.js
+++ plugins/vuex-persist.js
@@ -1,9 +1,22 @@
 import VuexPersistence from "vuex-persist";
 
+const clone = obj => JSON.parse(JSON.stringify(obj));
+
+// Return the version of the state that you want to keep
+// persisted in localStorage
+const reducer = state => {
+	let persistedState = clone(state);
+
+	delete persistedState.hidden;
+	delete persistedState.gradesIcon;
+	delete persistedState.unfolded;
+	delete persistedState.unfoldIcon;
+
+	return persistedState;
+};
+
 export default ({ store }) => {
 	window.onNuxtReady(() => {
-		new VuexPersistence({
-			/* your options */
-		}).plugin(store);
+		new VuexPersistence({ reducer }).plugin(store);
 	});
 };
